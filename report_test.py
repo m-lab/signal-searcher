e8b6de3dee72c1fe968c445d18efd54ce340cf9d
Peter Boothe <pboothe@google.com>
2016-08-04 15:01:58 -0400

    Added basic reporting.
    Cleaned up and added docstrings to everything, now passes lint.
    Everything is now Google Python Style Guide compliant.

--- report_test.py
+++ report_test.py
@@ -13,7 +13,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import datetime
 import unittest
+import netaddr
+import report
+
 
 class ReportTests(unittest.TestCase):
-  pass  # reports don't yet do anything, so they have no behavior to test
+
+  def setUp(self):
+    self.empty_report = report.Report()
+    self.problem = report.Problem(
+        netaddr.IPNetwork('10.0.0.0/8'),
+        datetime.timedelta(hours=48),
+        9,
+        'not really a problem')
+    self.single_report = report.Report([self.problem])
+
+  def test_size_makes_sense(self):
+    self.assertEqual(report._netblock_size(netaddr.IPNetwork('10.0.0.0/32')), 1)
+    self.assertEqual(
+        report._netblock_size(netaddr.IPNetwork('10.0.0.0/24')), 256)
+
+  def test_str(self):
+    self.assertEqual(
+        str(self.problem),
+        'Problem with 10.0.0.0/8 lasting for 2 days, 0:00:00 (severity 9): '
+        'not really a problem')
+
+  def test_cli_on_empty_report(self):
+    self.assertEqual(self.empty_report.cli_report(), 'No problems found.')
+
+  def test_cli_on_single_report(self):
+    self.assertEqual(len(self.single_report.cli_report().splitlines()), 2)
