cc55296a984d9baf4ad0b03c5f45d00a7d6fd870
Peter Boothe <pboothe@google.com>
2017-11-20 20:05:58 +0000

    mlabdata now divided into permanent and temp parts

--- mlabdata.py
+++ mlabdata.py
@@ -1,9 +1,16 @@
 import collections
 
+"""All of the datatypes that get passed around inside Signal Searcher."""
+
 InternetData = collections.namedtuple(
     'InternetData',
     ['key', 'table', 'time', 'upload', 'download', 'rtt', 'samples'])
 
+
+# Everything below this line is temporary and should be deleted when the
+# migration away from the spike is complete.
+
+# pylint disable=missing-docstring, no-self-use
 
 # Deprecated --- DO NOT USE
 MlabDataEntry = collections.namedtuple(
